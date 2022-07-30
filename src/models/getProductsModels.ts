68137ee6aedf8f14d6a66832fcd7093730859f5e
PatriciaDivi <patriciavianad@gmail.com>
2022-07-30 04:21:25 -0300

    feat: Criando o endpoint get para listar todos os '/products' :heavy_minus_sign: implementado na pasta 'interfaces' o arquivo 'AllProducts.ts' :heavy_minus_sign: implementado na pasta 'models' a connection e a query para listar todos os products :rocket:

--- src/models/getProductsModels.ts
+++ src/models/getProductsModels.ts
@@ -1,7 +1,11 @@
+// import { ResultSetHeader } from 'mysql2';
 import connection from './connection';
+import { AllProducts } from '../interfaces/AllProducts';
 
-const getProductsModels = async (): Promise<ProductModel[]> => {
-  
+const getProductsModels = async (): Promise<AllProducts[]> => {
+  const query = 'SELECT * FROM Trybesmith.Products';
+  const [result] = await connection.execute(query);
+  return result as AllProducts[];
 };
 
 export default getProductsModels;
