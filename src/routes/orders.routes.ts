f4fb47eddf882c7a073aeb7dc6815f3e32aa6689
PatriciaDivi <patriciavianad@gmail.com>
2022-08-01 19:30:43 -0300

    refactor: corrigindo do nome do arquivo 'getOrdersController.ts' dentro da pasta routes

--- src/routes/orders.routes.ts
+++ src/routes/orders.routes.ts
@@ -3,6 +3,6 @@ import controllers from '../controllers/index';
 
 const orders = Router();
 
-orders.post('/orders', controllers.postOrdersController);
+orders.post('/orders', controllers.getOrdersController);
 
 export default orders;
