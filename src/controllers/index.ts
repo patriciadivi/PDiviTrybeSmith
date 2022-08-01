0f7a7cf086c29c46c68a3ae0ea806a7b029b38ee
PatriciaDivi <patriciavianad@gmail.com>
2022-08-01 19:27:32 -0300

    refactor: corrigindo do nome do arquivo 'getOrdersController.ts'

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,9 +1,11 @@
 import postProductsController from './postProductsController';
 import getProductsController from './getProductsController';
 import postUsersController from './postUsersController';
+import getOrdersController from './getOrdersController';
 
 export default { 
   postProductsController,
   getProductsController,
   postUsersController,
+  getOrdersController,
 };
