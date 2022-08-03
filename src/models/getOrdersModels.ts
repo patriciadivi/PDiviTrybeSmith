d01938bfd4564d96437fbcdef18ea318ecadb925
PatriciaDivi <patriciavianad@gmail.com>
2022-08-02 23:15:06 -0300

    feat: impementa do endpoint na rota de '/orders' listando 'id' da orders , 'userId' de users e a quantidade de produtos em um array em 'productsId'da products

--- src/models/getOrdersModels.ts
+++ src/models/getOrdersModels.ts
@@ -1,10 +1,21 @@
+import { AllOrders } from '../interfaces/AllOrders';
 import connection from './connection';
-import { AllProducts } from '../interfaces/AllProducts';
 
-const getOrdersModels = async (): Promise<AllProducts[]> => {
-  const query = 'SELECT * FROM Trybesmith.Orders';
+const getOrdersModels = async (): Promise<AllOrders[] | null > => {
+  const query = `
+  SELECT Trybesmith.Orders.id,
+  Trybesmith.Orders.userId as userId,
+  JSON_ARRAYAGG(Trybesmith.Products.id) as productsIds
+  FROM Trybesmith.Orders as Orders 
+  INNER JOIN Trybesmith.Products as Products
+  ON Trybesmith.Orders.id = Trybesmith.Products.orderId
+  GROUP BY Trybesmith.Orders.id
+  ORDER BY Trybesmith.Orders.userId;
+  `;
   const [result] = await connection.execute(query);
-  return result as AllProducts[];
+  console.log(result);
+  
+  return result as AllOrders[];
 };
 
 export default getOrdersModels;
